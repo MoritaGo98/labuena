920c5e7d086bc0d6df9482a7ab89a084605ddec4
MoritaGo98 <antoniomontoyaam54@gmail.com>
2025-03-14 12:09:52 -0600

    Add files via upload

---
+++ A donde me dirijo.js
@@ -0,0 +1,12 @@
+let trakas = 'vino'
+if(trakas == 'cerveza'){
+   console.log('Dirigete a la barra')
+}else if(trakas == 'vino'){
+    console.log('Dirigete a la barra')
+}else if(trakas == 'agua'){
+    console.log('Dirigete a la tienda')
+}else if(trakas == 'comida'){
+    console.log('Dirigete a la tienda')
+}else{
+    console.log('Dirigete al Aurrera papito')
+}
